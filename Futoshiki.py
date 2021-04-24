7467612f47032c013ebfcf63a348e6e60d9c8ea1
Mateo <mc7212@nyu.edu>
2021-04-23 23:17:13 -0400

    Added CSP class and some functions

--- Futoshiki.py
+++ Futoshiki.py
@@ -1,5 +1,83 @@
 import sys
 
+# Checks if all values in a list are different (Except for 0)
+def checkDiff(elems):
+    for elem in elems:
+        #Don't check for 0
+        if(elem == "0"):
+            continue
+
+        # Returns False if a duplicate is found
+        if elems.count(elem) > 1:
+            return False
+
+    # Returns true all elements are "0" or there isn't a duplicate
+    return True
+
+# AllDiff constraint for row and col
+def allDiff(row, col, board):
+    row_elems = []
+    col_elems = []
+
+    for c in range(0, 6, 1):
+        row_elems.append(board[row][c])
+
+    for r in range(0, 6, 1):
+        col_elems.append(board[r][col])
+
+    return checkDiff(row_elems) and checkDiff(col_elems)
+
+
+# initial: Initial Board (2D Array)
+# horiz_constraints: Horizontal Constraints (2D Array)
+# vert_constraints: Vertical Constraints (2D Array)
+# variables: All variables in form xij (List)
+# domains: Domains for all variables (Dictionary)
+class CSP:
+    def __init__(self, initial, horiz_constraints, vert_constraints):
+        self.initial = initial
+        self.horiz_constraints = horiz_constraints
+        self.vert_constraints = vert_constraints
+        self.variables = []
+        for i in range(0, 6, 1):
+            for j in range(0, 6, 1):
+                self.variables.append("x" + str(i) + str(j))
+        self.domains = {}
+        for var in self.variables:
+            self.domains[var] = [1, 2, 3, 4, 5, 6]
+
+
+    # Updates neighbors of variable at row,col
+    # Used for forward checking
+    def updateNeighborDomains(self, row, col):
+        num = self.initial[row][col]
+
+        # Removes value at i,j from its neighbors domains
+        # Column neighbors
+        for c in range(0, 6, 1):
+            var = "x" + str(row) + str(c)
+            if int(num) in self.domains[var]:
+                self.domains[var].remove(int(num))
+
+        # Row neighbors
+        for r in range(0, 6, 1):
+            var = "x" + str(r) + str(col)
+            if int(num) in self.domains[var]:
+                self.domains[var].remove(int(num))
+
+        #STILL NEED TO UPDATE NEIGHBORS BASED ON VERTICAL AND HORIZONTAL CONSTRAINTS
+
+
+    # Updates domains of neighbors for intial values
+    def forwardCheck(self):
+        for i in range(0, 6, 1):
+            for j in range(0, 6, 1):
+                if self.initial[i][j] != "0":
+                    self.updateNeighborDomains(i, j)
+
+
+
+
 #---------------------------------Main------------------------------
 initial = []
 horiz_ineq = []
@@ -24,12 +102,12 @@ input_str = input_file.read()
 split_input = input_str.split('\n')
 
 # Clean the output file
-file = open("SampleOutput.txt","r+")
-file.truncate(0)
-file.close()
+#file = open("SampleOutput.txt","r+")
+#file.truncate(0)
+#file.close()
 
 # Open output file for writing
-sys.stdout = open("SampleOutput.txt", "w")
+#sys.stdout = open("SampleOutput.txt", "w")
 
 switch = 0
 curr_elem = ""
@@ -69,5 +147,22 @@ for row in range(0, 5, 1):
         print(vert_ineq[row][col], end = " ")
     print()
 
+print()
+
+# Solution for CSP initialized
+solution = CSP(initial, horiz_ineq, vert_ineq)
+
+# Apply forward checking before starting search
+solution.forwardCheck()
+
 input_file.close()
 sys.stdout.close()
+
+
+# TODO
+# Finish updateDomains (need to update based on inequalities)
+# Create backtracking algorithm
+# Implement Select-Unassigned-Value
+# Implement Minimum Remaining Value and Degree Heuristics (used in Select-Unassigned-Value)
+# Clean up the file
+# Create PDF
